7620902f7d39fc956e5166db115564aa9f7f548d
ShovalElhaiany <shoval.elhaiany@gmail.com>
2024-06-28 19:36:32 +0300

    Registration fix

--- client/.venv/Scripts/activate.bat
+++ client/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\shova\Desktop\FlightSystemProject\client\.venv
+set VIRTUAL_ENV=C:\Users\amits\Desktop\Shoval\FlightSystemProject\client\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
